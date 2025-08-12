812ba685e2e14c77546c1e8427e28b507aa0e9c9
Hector-SWAT <hectorhernadez51@gmail.com>
2025-08-11 19:13:38 -0600

    Add netlify configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'static', // Importante para Netlify
+  site: 'https://tu-sitio.netlify.app', // Opcional, actualízalo después
   integrations: [react()],
 });
